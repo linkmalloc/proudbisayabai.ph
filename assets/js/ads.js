2fe2a65c5cdbd37e4bd30ce51086735050f31726
ian@linkamlloc <linkmallocinc@gmail.com>
2021-06-27 14:57:55 +0800

    added initial ads

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -2,6 +2,21 @@
     'use strict';
     let adsIn = [];
     let ads = () =>{
+
+        let populate =  async (key)=>{
+            try{
+            let AD = JSON.parse(localStorage.getItem(key));
+            if(!!AD){
+                $('.sponsor a').attr('href', AD.url )
+                $('.sponsor .img').css('background-image', `url (${AD.img} )`);
+            }
+            
+            }catch(e){
+                // debugger;
+            }
+
+        }
+
         return {
             init: async ()=>{
                 let response = await fetch(__ADS_URL);
@@ -10,6 +25,9 @@
                 data.forEach( (ad) =>{
                     localStorage.setItem(ad.id, JSON.stringify(ad));
                     adsIn.push(ad.id);
+
+                    let ADtoShow = localStorage.getItem('ADtoShow');
+                    populate(ADtoShow);
                 });
             }
         }
@@ -18,9 +36,13 @@
         // 
     }
 
-    $(document).ready(()=>{
+    $(window).ready(()=>{
         // fetch json ads
-        ads().init();
+        let ADS = ads();
+        localStorage.setItem('ADtoShow','https://cebuhomepages.com/');
+        ADS.init();
+        
+
         // populate related posts
         // populate popup
     })
